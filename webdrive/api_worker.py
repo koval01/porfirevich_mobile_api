3f8352fb2dc0682157a48dd1675fa79e6ce619aa
koval01 <koval.developer@gmail.com>
2021-02-23 22:03:01 +0200

    update

--- webdrive/api_worker.py
+++ webdrive/api_worker.py
@@ -36,35 +36,17 @@ def export_data(array):
                     <div style="width: calc(1.0 - 90px); float: left; ">
                         <label class="status" data-toggle="tooltip" data-placement="top" data-original-title="1" style="color: 2"><svg style="filter: invert(0.8);" class="svg-inline--fa fa-circle fa-w-16" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path></svg></label>
                         <label class="username">Пользовательская запись</label><br>
-                        <label class="city">%s<br><br><i><u>Опубликовано %s</u></i><br>
-                        <u>%s</u> ❤️<br></label>
+                        <label class="city">%s<br><br><u>%s</u> ❤️<br></label>
                     </div>
                 </div>
             </div>
     """
     data_array = []
     for i in array:
-        template_ = template % (str(i[0]), str(i[2]), str(i[1]))
+        template_ = template % (str(i[0]), str(i[1]))
         data_array.append(template_)
 
     return ''.join(data_array)
-
-
-def month_convert(string):
-    en_mon = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
-              'October', 'November', 'December']
-    ru_mon = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября',
-              'октября', 'ноября', 'декабря']
-    for i in en_mon:
-        string = string.replace(i, ru_mon[en_mon.index(i)])
-    return string
-
-
-def time_editor(time):
-    time = str(time)[:-5]
-    d = datetime.datetime.strftime(time, "%d %B %Y г. %H:%M")
-    field = month_convert(d)
-    return field
 
 
 def api_get_data() -> str:
@@ -75,9 +57,8 @@ def api_get_data() -> str:
     for i in data['data']:
         d = decode_story_string(i['content'])
         l = i['likesCount']
-        u = i['updatedAt']
-        t = time_editor(u)
-        a = [d, l, t]
+        # u = i['updatedAt']
+        a = [d, l]
         array_data.append(a)
     
     result = export_data(array_data)
